dde8b4953076984e5668cf35fb15e26847cc197b
dala_bila <dalahhashlamoon@gmail.com>
2023-12-17 23:38:23 +0200

    hash the password

--- router/userSignup.ts
+++ router/userSignup.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import User from '../models/user'; // Adjust the path as needed
+import bycypt from 'bcrypt'
 
 const router = express.Router();
 
@@ -9,8 +10,9 @@ router.post('/', async (req: Request, res: Response) => {
   const { firstname, lastname, email, password, phoneNumber } = req.body;
 
   try {
+    const hashpassword = await bycypt.hash(password,10)
     // Create a new user in the database
-    const newUser = new User({ firstname, lastname, email, password, phoneNumber });
+    const newUser = new User({ firstname, lastname, email, password : hashpassword, phoneNumber });
     await newUser.save();
 
     // Send a success response
