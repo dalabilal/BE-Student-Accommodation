f8e33bee6e6b0fa609c1d939dd1b98740eabc94f
dala_bila <dalahhashlamoon@gmail.com>
2024-02-11 20:09:24 +0200

    add payment form

--- models/payInfo.ts
+++ models/payInfo.ts
@@ -1,14 +1,30 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
 interface IPayment extends Document {
+  holdername: string;
+  cardnum: { iv: string; encryptedText: string; tag: string };
+  cvv: { iv: string; encryptedText: string; tag: string };
+  expDate: string;
   status: boolean;
   useid: string;
   housingId:string;
 }
 
 const paymentSchema: Schema = new Schema({
-  status: { type: Boolean, required: true },
-  useid: { type: String, required: true },
+  holdername: { type: String,  },
+  cardnum: {
+    iv: { type: String,  },
+    encryptedText: { type: String,  },
+    tag: { type: String,  },
+  },
+  cvv: {
+    iv: { type: String,},
+    encryptedText: { type: String,},
+    tag: { type: String},
+  },
+  expDate: { type: String,  },
+  status: { type: Boolean,  },
+  useid: { type: String,  required: true },
   housingId: { type: String, required: true },
   ownerId: { type: String, required: true },
 });
--- router/payment.ts
+++ router/payment.ts
@@ -2,10 +2,64 @@ import express, { Request, Response } from 'express';
 import Payment from '../models/payInfo';
 import Logs from '../models/logsfile';
 import Users from '../models/userLogin';
+import crypto from 'crypto';
 
 const router = express.Router();
-
+function encrypt(text: string): { iv: string; encryptedText: string; tag: string } {
+  const iv = crypto.randomBytes(16).toString('hex');
+  const cipher = crypto.createCipheriv('aes-256-gcm', Buffer.from(process.env.ENCRYPTION_KEY || ''), Buffer.from(iv, 'hex'));
+  let encrypted = cipher.update(text, 'utf-8', 'hex');
+  encrypted += cipher.final('hex');
+  const tag = cipher.getAuthTag().toString('hex');
+  return { iv, encryptedText: encrypted, tag };
+}
+function decrypt(iv: string, encryptedText: string, tag: string): string {
+  const decipher = crypto.createDecipheriv('aes-256-gcm', Buffer.from(process.env.ENCRYPTION_KEY || ''), Buffer.from(iv, 'hex'));
+  decipher.setAuthTag(Buffer.from(tag, 'hex'));
+  let decrypted = decipher.update(encryptedText, 'hex', 'utf-8');
+  decrypted += decipher.final('utf-8');
+  return decrypted;
+}
 router.post('/', async (req: Request, res: Response) => {
+  
+  try {
+    const holdername: string = req.body.holdername;
+
+    const { iv: cardnumIV, encryptedText: cardnum, tag: cardnumTag } = encrypt(req.body.cardnum);
+    const { iv: cvvIV, encryptedText: cvv, tag: cvvTag } = encrypt(req.body.cvv);
+    const expDate: string = req.body.expDate;
+    const useid: string = req.body.useid;
+    const housingId: string = req.body.housingId;
+    const ownerId: string = req.body.ownerId;
+
+    const payment = {
+      holdername,
+      cardnum: { iv: cardnumIV, encryptedText: cardnum, tag: cardnumTag },
+      cvv: { iv: cvvIV, encryptedText: cvv, tag: cvvTag },
+      expDate,
+      useid,
+      housingId,
+      ownerId
+    }
+    const newPayment = new Payment(payment);
+    const savedPayment = await newPayment.save();
+    const user = await Users.findOne({ userID : newPayment?.useid });
+
+    let userLogs = new Logs({
+      userID: newPayment.useid,
+      date: new Date(),
+      name: user?.email || "df",
+      actionType:"Booking",
+   });
+
+    await userLogs.save();
+    res.status(201).json({ message: 'Payment successful', savedPayment });
+  } catch (error) {
+    console.error('Error processing payment:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+router.post('/google', async (req: Request, res: Response) => {
   
   try {
     const status = req.body.status;
@@ -26,7 +80,7 @@ router.post('/', async (req: Request, res: Response) => {
     let userLogs = new Logs({
       userID: newPayment.useid,
       date: new Date(),
-      name: user?.email,
+      name: user?.email || "fgsd",
       actionType:"Booking",
    });
 
